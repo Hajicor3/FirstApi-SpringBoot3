1df656b65150fda303bc3bdc1d1986bafbad1e1c
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2024-11-11 16:17:41 -0300

    Entity client adjustment and client repository

--- src/main/java/com/projetoSpring/hajiApi/entities/Client.java
+++ src/main/java/com/projetoSpring/hajiApi/entities/Client.java
@@ -1,5 +1,6 @@
 package com.projetoSpring.hajiApi.entities;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
@@ -9,29 +10,28 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
 @Entity (name = "tb_users")
-public class Client {
-	
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
+	private String password;
 	private String email;
 	private String phoneNumber;
 	private String adress;
 	private String cep;
 	private LocalDate birthDate;
-	
-	public Client() {
-	}
-	
-	
 	
 }
---
+++ src/main/java/com/projetoSpring/hajiApi/repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.projetoSpring.hajiApi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoSpring.hajiApi.entities.Client;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
