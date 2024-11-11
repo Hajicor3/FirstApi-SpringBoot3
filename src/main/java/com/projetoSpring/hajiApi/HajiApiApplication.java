f50da2fec2f500eea4613b4d367f8fd5d9ff2148
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2024-11-11 11:30:55 -0300

    first commit

---
+++ src/main/java/com/projetoSpring/hajiApi/HajiApiApplication.java
@@ -0,0 +1,13 @@
+package com.projetoSpring.hajiApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoSpring/hajiApi/HajiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoSpring.hajiApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HajiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
