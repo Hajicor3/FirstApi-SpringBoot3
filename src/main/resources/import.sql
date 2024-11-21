20c5ca5143f5835402a1c780f61d5fc225e134eb
Hajicor3 <gabriel.mirandawv@outlook.com>
2024-11-21 13:30:24 -0300

    docker compose and dev profile

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO tb_client (birth_date, adress, cep, email, name, password, phone_number) VALUES ('1980-10-07', 'passagem joao cabelo, 2000', '88902', 'joao@gmail.com','joao','1234565', '999999-9999');
+INSERT INTO tb_client (birth_date, adress, cep, email, name, password, phone_number) VALUES ('1980-12-10', 'passagem joana cabelo, 292030', '991002', 'joana@gmail.com','joana','1234565', '999999-9999');
+INSERT INTO tb_client (birth_date, adress, cep, email, name, password, phone_number) VALUES ('1950-08-02', 'rua groove street, 3', '66625410', 'Cj@gmail.com','Carl Jonson','1234565', '999999-9999');
+INSERT INTO tb_client (birth_date, adress, cep, email, name, password, phone_number) VALUES ('2000-07-01', 'rua newyork, 100', '2002607', 'ana@gmail.com','ana maria','1234565', '999999-9999');
